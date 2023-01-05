1aafd5a8bf45c3d77a063c97764497166d77731b
tot0p <thomas.lemaitre.78420@gmail.com>
2023-01-05 11:22:51 +0100

    update

--- generator/main.py
+++ generator/main.py
@@ -1,6 +1,32 @@
 import json
-
+import os
 
 def main():
-    with open('all.json', 'r') as f:
+    with open('../data.json', 'r', encoding='utf-8') as f:
         data = json.load(f)
+        for item in range(len(data)):
+            data[item]['id'] = item
+    create_dir('../api')
+    write_file('../api/all.json', json.dumps(data, ensure_ascii=False, indent=4))
+    create_By_Id(data)
+
+
+def create_By_Id(data: list):
+    for item in data:
+        create_dir(f'../api/{item["id"]}')
+        write_file(f'../api/{item["id"]}/index.json', json.dumps(item, ensure_ascii=False, indent=4))
+
+
+def create_dir(dir_name: str):
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
+
+
+def write_file(file_name: str, data: str):
+    with open(file_name, 'w', encoding='utf-8') as f:
+        f.write(data)
+
+if __name__ == '__main__':
+    main()
+    
+
