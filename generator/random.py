d276d14e481b5b55f6cd1c48dee27f3031b7560e
tot0p <thomas.lemaitre.78420@gmail.com>
2023-01-05 12:42:06 +0100

    update

--- generator/random.py
+++ generator/random.py
@@ -11,16 +11,14 @@ def main() :
     if os.path.exists('docs/api/joke') :
         with open('data.json', 'r', encoding='utf-8') as f :
             data = json.load(f)
-            random.shuffle(data)
             create_dir('docs/api/joke/random')
-            write_file('docs/api/joke/random/index.json', json.dumps(data[0], ensure_ascii=False, indent=4))
+            write_file('docs/api/joke/random/index.json', json.dumps(data[random.randint(0,len(data)-1)], ensure_ascii=False, indent=4))
         if os.path.exists('docs/api/jokes/lang'):
             for lang in os.listdir('docs/api/jokes/lang'):
                 with open(f'docs/api/jokes/lang/{lang}/index.json', 'r', encoding='utf-8') as f:
                     data = json.load(f)
-                    random.shuffle(data)
                     create_dir(f'docs/api/joke/random/{lang}')
-                    write_file(f'docs/api/joke/random/{lang}/index.json', json.dumps(data[0], ensure_ascii=False, indent=4))
+                    write_file(f'docs/api/joke/random/{lang}/index.json', json.dumps(data[random.randint(0,len(data)-1)], ensure_ascii=False, indent=4))
             
 
 
