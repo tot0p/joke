cb2c625a91183e8a9e884fefe8b2b2e280d2e011
tot0p <thomas.lemaitre.78420@gmail.com>
2023-01-05 11:34:36 +0100

    update

--- generator/main.py
+++ generator/main.py
@@ -2,19 +2,38 @@ import json
 import os
 
 def main():
-    with open('../data.json', 'r', encoding='utf-8') as f:
+    with open('data.json', 'r', encoding='utf-8') as f:
         data = json.load(f)
         for item in range(len(data)):
             data[item]['id'] = item
-    create_dir('../api')
-    write_file('../api/all.json', json.dumps(data, ensure_ascii=False, indent=4))
+    create_dir('api')
     create_By_Id(data)
+    create_All(data)
+    create_By_lang(data)
+    create_By_author(data)
 
+
+def create_All(data: list):
+    create_dir('api/items')
+    write_file('api/items/index.json', json.dumps(data, ensure_ascii=False, indent=4))
 
 def create_By_Id(data: list):
     for item in data:
-        create_dir(f'../api/{item["id"]}')
-        write_file(f'../api/{item["id"]}/index.json', json.dumps(item, ensure_ascii=False, indent=4))
+        create_dir('api/id')
+        create_dir(f'api/id/{item["id"]}')
+        write_file(f'api/id/{item["id"]}/index.json', json.dumps(item, ensure_ascii=False, indent=4))
+
+def create_By_lang(data: list):
+    for item in data:
+        create_dir('api/lang')
+        create_dir(f'api/lang/{item["lang"]}')
+        write_file(f'api/lang/{item["lang"]}/index.json', json.dumps(item, ensure_ascii=False, indent=4))
+
+def create_By_author(data: list):
+    for item in data:
+        create_dir('api/author')
+        create_dir(f'api/author/{item["author"]}')
+        write_file(f'api/author/{item["author"]}/index.json', json.dumps(item, ensure_ascii=False, indent=4))
 
 
 def create_dir(dir_name: str):
