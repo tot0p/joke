242cb45c77185fe064a039dd002c9e620ad8a57f
tot0p <thomas.lemaitre.78420@gmail.com>
2023-01-05 12:44:17 +0100

    update

--- generator/random.py
+++ generator/random.py
@@ -1,6 +1,6 @@
 import os
 import json
-import random
+from random import randint
 
 from lib import create_dir, write_file
 
@@ -12,13 +12,13 @@ def main() :
         with open('data.json', 'r', encoding='utf-8') as f :
             data = json.load(f)
             create_dir('docs/api/joke/random')
-            write_file('docs/api/joke/random/index.json', json.dumps(data[random.randint(0,len(data)-1)], ensure_ascii=False, indent=4))
+            write_file('docs/api/joke/random/index.json', json.dumps(data[randint(0,len(data)-1)], ensure_ascii=False, indent=4))
         if os.path.exists('docs/api/jokes/lang'):
             for lang in os.listdir('docs/api/jokes/lang'):
                 with open(f'docs/api/jokes/lang/{lang}/index.json', 'r', encoding='utf-8') as f:
                     data = json.load(f)
                     create_dir(f'docs/api/joke/random/{lang}')
-                    write_file(f'docs/api/joke/random/{lang}/index.json', json.dumps(data[random.randint(0,len(data)-1)], ensure_ascii=False, indent=4))
+                    write_file(f'docs/api/joke/random/{lang}/index.json', json.dumps(data[randint(0,len(data)-1)], ensure_ascii=False, indent=4))
             
 
 
